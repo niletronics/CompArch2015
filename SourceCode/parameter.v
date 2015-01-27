448f6079a305815702027c67645d157f6d3cdc13
Nilesh <niletronics@live.in>
2015-01-26 21:32:34 -0800

    Parameters

---
+++ SourceCode/parameter.v
@@ -0,0 +1,9 @@
+
+parameter AND 				= 4'b0;
+parameter TAD				= 4'b1;
+parameter ISZ				= 4'b2; 
+parameter DCA				= 4'b3;
+parameter JMS				= 4'b4;
+parameter JMP				= 4'b5;
+parameter IO				= 4'b6;
+parameter ΜINSTRUCTIONS		= 4'b7;
