764ec51752aedcb22bee4e13b690dc1d5c7c0f93
Nilesh <niletronics@live.in>
2015-02-14 02:32:19 -0800

    grp2uinstr ready for review comments and testing
    Hi,
    Group-2 MicroInstructions are ready for testing and review comments.
    
    I have put comments, however feel free to post questions as you review
    the code
    
    For testing : we will have to make combination and check.

--- SourceCode/group2microinstructions.sv
+++ SourceCode/group2microinstructions.sv
@@ -36,52 +36,62 @@ parameter i_SKON = 12'o6000;				//Skip if the interrupt system is on and turn th
 parameter i_ION = 12'o6001;					//Execute the next instruction then turn the interrupt system on
 parameter i_IOF = 12'o6002;					//Turn the interrupt system off
 //--------------------------Group 1 Microinstructions (Bit 3 = 0)
-reg NOP;					//No Operation
-reg CLA;					//CLear Accumulator (1)
-reg CLL;					//CLear Link (1)
-reg CMA;					//CoMplement Accumulator (2)
-reg CML;					//CoMplement Link (2)
-reg IAC;					//Increment ACumulator (3)
-reg RAR;					//Rotate Accumulator and link Right (4)
-reg RTR;					//Rotate accumulator and link Right Twice (4)
-reg RAL;					//Rotate Accumulator and link Left (4)
-reg RTL;					//Rotate Accumulator and link left Twice (4)
+reg NOP = 1'b0;					//No Operation
+reg CLA = 1'b0;					//CLear Accumulator (1)
+reg CLL = 1'b0;					//CLear Link (1)
+reg CMA = 1'b0;					//CoMplement Accumulator (2)
+reg CML = 1'b0;					//CoMplement Link (2)
+reg IAC = 1'b0;					//Increment ACumulator (3)
+reg RAR = 1'b0;					//Rotate Accumulator and link Right (4)
+reg RTR = 1'b0;					//Rotate accumulator and link Right Twice (4)
+reg RAL = 1'b0;					//Rotate Accumulator and link Left (4)
+reg RTL = 1'b0;					//Rotate Accumulator and link left Twice (4)
 //-------Group 2 Microinstructions (Bit 3 = 1, Bit 11 = 0) 
-reg SMA;					//Skip on Minus Accumulator (1)
-reg SZA;					//Skip on Zero Accumulator (1)
-reg SNL;					//Skip on Nonzero Link (1)
-reg SPA;					//Skip on Positive Accumulator (1)
-reg SNA;					//Skip on Nonzero Accumulator (1)
-reg SZL;					//Skip on Zero Link (1)
-reg SKP;					//SKiP always (1)
-reg CLA;					//CLear Accumulator (2)
-reg OSR;					//Or Switch Register with accumulator (3)
-reg HLT;					//HaLT (3)
+reg SMA = 1'b0;					//Skip on Minus Accumulator (1)
+reg SZA = 1'b0;					//Skip on Zero Accumulator (1)
+reg SNL = 1'b0;					//Skip on Nonzero Link (1)
+reg SPA = 1'b0;					//Skip on Positive Accumulator (1)
+reg SNA = 1'b0;					//Skip on Nonzero Accumulator (1)
+reg SZL = 1'b0;					//Skip on Zero Link (1)
+reg SKP = 1'b0;					//SKiP always (1)
+reg CLA = 1'b0;					//CLear Accumulator (2)
+reg OSR = 1'b0;					//Or Switch Register with accumulator (3)
+reg HLT = 1'b0;					//HaLT (3)
 //-------Group 3 Microinstructions (Bit 3 = 1, Bit 11 = 1) 
-reg CLA;					//CLear Accumulator (1)
-reg MQL;					//Load MQ register from AC and Clear AC (2); C(MQ) <- C(AC); C(AC) <- 0;
-reg MQA;					//Or AC with MQ register (2) ; C(AC) <- C(AC) Or C(MQ)
-reg SWP;					//SWap AC and MQ registers (3)
-reg CAM;					//Clear AC and MQ registers (3)
-
+reg CLA = 1'b0;					//CLear Accumulator (1)
+reg MQL = 1'b0;					//Load MQ register from AC and Clear AC (2); C(MQ) <- C(AC); C(AC) <- 0;
+reg MQA = 1'b0;					//Or AC with MQ register (2) ; C(AC) <- C(AC) Or C(MQ)
+reg SWP = 1'b0;					//SWap AC and MQ registers (3)
+reg CAM = 1'b0;					//Clear AC and MQ registers (3)
+reg ORSubgroup =1'b0;			// Condition for ORSubGroup 1'b1 = True, 1'b0 = False
+reg ANDSubgroup =1'b0;			// Condition for ANDSubgroup  1'b1 = True, 1'b0 = False
 
 task Group2MicroInstructions;   
 begin
-
-if(my_memory[PC] ==? 12'b111_1?0_001_??0) begin	SKP	<=1'b1; $display("SKP"); end
-if(my_memory[PC] ==? 12'b111_11?_???_??0) begin	CLA	<=1'b1; $display("CLA"); end
-if(my_memory[PC] ==? 12'b111_1??_???_1?0) begin	OSR	<=1'b1; $display("OSR"); end
-if(my_memory[PC] ==? 12'b111_1??_???_?10) begin	HLT	<=1'b1; $display("HLT"); end
-if(my_memory[PC] ==? 12'b111_1?1_??0_??0) begin	SMA	<=1'b1; $display("SMA"); end
-if(my_memory[PC] ==? 12'b111_1??_1?0_??0) begin	SZA	<=1'b1; $display("SZA"); end
-if(my_memory[PC] ==? 12'b111_1??_?10_??0) begin	SNL	<=1'b1; $display("SNL"); end
-if(my_memory[PC] ==? 12'b111_100_000_000) begin	NOP	<=1'b1; $display("NOP"); end
-if(my_memory[PC] ==? 12'b111_1?1_??1_??0) begin	SPA	<=1'b1; $display("SPA"); end
-if(my_memory[PC] ==? 12'b111_1??_1?1_??0) begin	SNA	<=1'b1; $display("SNA"); end
-if(my_memory[PC] ==? 12'b111_1??_?11_??0) begin	SZL	<=1'b1; $display("SZL"); end
-
-if((SMA && AC[0]=1'b1) || (SZA && AC == 0) || (SNL && LinkBit==1'b1) PC++;
- 
+	if(my_memory[PC] ==? 12'b111_1?0_001_??0) begin	SKP	<=1'b1; /*$display("SKP");*/ end
+	if(my_memory[PC] ==? 12'b111_11?_???_??0) begin	CLA	<=1'b1; /*$display("CLA");*/ end
+	if(my_memory[PC] ==? 12'b111_1??_???_1?0) begin	OSR	<=1'b1; /*$display("OSR");*/ end
+	if(my_memory[PC] ==? 12'b111_1??_???_?10) begin	HLT	<=1'b1; /*$display("HLT");*/ end
+	if(my_memory[PC] ==? 12'b111_1?1_??0_??0) begin	SMA	<=1'b1; /*$display("SMA");*/ end
+	if(my_memory[PC] ==? 12'b111_1??_1?0_??0) begin	SZA	<=1'b1; /*$display("SZA");*/ end
+	if(my_memory[PC] ==? 12'b111_1??_?10_??0) begin	SNL	<=1'b1; /*$display("SNL");*/ end
+	if(my_memory[PC] ==? 12'b111_100_000_000) begin	NOP	<=1'b1; /*$display("NOP");*/ end
+	if(my_memory[PC] ==? 12'b111_1?1_??1_??0) begin	SPA	<=1'b1; /*$display("SPA");*/ end
+	if(my_memory[PC] ==? 12'b111_1??_1?1_??0) begin	SNA	<=1'b1; /*$display("SNA");*/ end
+	if(my_memory[PC] ==? 12'b111_1??_?11_??0) begin	SZL	<=1'b1; /*$display("SZL");*/ end
+	//Condition checking for SubGroup
+	if((SMA && AC[0]==1'b1) || (SZA && AC == 12'b0) || (SNL && LinkBit==1'b1)) ORSubgroup <=1'b1;
+	if((SPA && AC[0]==1'b0) && (SNA && AC != 12'b0) && (SZA && LinkBit==1'b0)) ANDSubgroup <=1'b1;
+	
+	if(ORSubgroup || ANDSubgroup || SKP) PC++; 						//OR SubGroup, later combining common case
+	//if(ANDSubgroup) PC++;						// AND SubGroup
+	//if(SKP) PC++;								// Priority(1)
+	if(CLA) AC = 12'b0;							// Priority (2)
+	if(OSR) AC = (AC | SR);						// Priority (3)
+	if(HLT);									// Priority (3) //Assuming HLT should be executed as the last instruction 
+endtask
+	
+	
  /*
 if(my_memory[PC][8] == 1'b0) begin						// OR SubGroup
 	if(my_memory[PC][5] == 1'b1 && AC[0] == 1'b1)   	//Skip Minus Accumulator
@@ -94,7 +104,7 @@ if(my_memory[PC][8] == 1'b0) begin						// OR SubGroup
 end
 */
 
-if(
+/*
 if(my_memory[PC][8] == 1'b1) begin												//AND subgroup
 	if(my_memory[PC] == 12'b111_101_001_000 && AC[0] == 1'b0)					//Skip Positive Accumulator
 		PC = PC + 1'b1;
@@ -129,5 +139,4 @@ if(my_memory[PC] == 12'b111_100_000_100)  									//OR Accumulator with Switch 
 	AC = (AC | SR);
 if(my_memory[PC] == 12'b111_100_000_010)   									// Halt
 	exit = 1'b1; // Exit to be defined
-end                       
-endtask
+end  */                    
